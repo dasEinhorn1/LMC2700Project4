826f97a37cbb0522f58f7121b400f75f910c116d
Adam Hayward <achayward1@gmail.com>
2017-10-31 00:44:23 -0400

    began work on site template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LMC2700 Project 4
+This project attempts to generate and describe modern art in a gallery-like
+setting using 3 sources of data: Flickr, Wordnik, and Stripe
--- index.php
+++ index.php
@@ -2,6 +2,42 @@
 <html>
   <head>
     <title><?php echo "Index"?></title>
+    <script src="https://use.fontawesome.com/687ab5b4bb.js"></script>
+    <link href="main.css" rel="stylesheet" type="text/css"/>
   </head>
-  <h1>The site is running properly.</h1>
+  <body>
+    <header>
+      <section class="title">
+        <h1>MOD</h1>
+        <h1>ERN</h1>
+        <h1 class="emphasize">ART</h1>
+      </section>
+      <nav>
+        <!-- Regenerate the page -->
+        <a href="#">View</a>
+        <!-- View the about section -->
+        <a href="#">About</a>
+      </nav>
+    </header>
+    <main>
+      <section class="art-frame">
+        <img src="img/test.jpg"/>
+        <span class="details-btn" data-to="aside">
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </span>
+      </section>
+      <aside>
+        <h3>Art Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <button>Action!</button>
+      </aside>
+    </main>
+  </body>
 </html>
